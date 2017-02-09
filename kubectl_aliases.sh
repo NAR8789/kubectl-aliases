12c4465bb04795554dc46abcf567d13d188521b2
xiao.fan <xiao.fan@workday.com>
2017-02-09 08:56:49 -0800

    shortcut for switching contexts

--- kubectl_aliases.sh
+++ kubectl_aliases.sh
@@ -1,5 +1,6 @@
 alias k=kubectl
 alias kg='kubectl get'
 alias kgp='kubectl get pods'
+alias kctx='kubectl config use-context'
 alias ksc='kubectl config set-context $(kubectl config current-context)'
 alias kns='ksc --namespace'
