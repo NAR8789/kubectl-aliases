cfa4bb2b8d6d63d527cb3c21471ca78681d03fc0
xiao.fan <xiao.fan@workday.com>
2017-02-09 09:22:24 -0800

    alias to get "all", for my current definition of "all"

--- kubectl_aliases.sh
+++ kubectl_aliases.sh
@@ -1,6 +1,7 @@
 alias k=kubectl
 alias kg='kubectl get'
 alias kgp='kubectl get pods'
+alias kgall='kubectl get ingress,service,deployment,pod'
 alias kctx='kubectl config use-context'
 alias ksc='kubectl config set-context "$(kubectl config current-context)"'
 alias kns='ksc --namespace'
