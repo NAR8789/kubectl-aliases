b950298d2a58d1926731fcbbdbfd3d27aeb16fa6
xiao.fan <xiao.fan@workday.com>
2017-02-09 09:30:35 -0800

    alias to get all namespaces

--- kubectl_aliases.sh
+++ kubectl_aliases.sh
@@ -1,6 +1,7 @@
 alias k=kubectl
 alias kg='kubectl get'
 alias kgp='kubectl get pods'
+alias kgns='kubectl get namespaces'
 alias kgall='kubectl get ingress,service,deployment,pod'
 alias kctx='kubectl config use-context'
 alias ksc='kubectl config set-context "$(kubectl config current-context)"'
